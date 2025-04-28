c468e6b7d0a16141f59621008b2cd55110f10003
Barbora Trnkova <barbortrnkova@gmail.com>
2025-04-28 13:25:20 +0200

    cvicenie

--- fibo.py
+++ fibo.py
@@ -1,5 +1,17 @@
 
+def recursive_nth_fibo(cislo):
+    if cislo == 0:
+        return 0
+    elif cislo == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(cislo - 1) + recursive_nth_fibo(cislo-2)
+
 def main():
+    n = int(input("Počet členov Fibonacciho postupnosti:"))
+    sekvencia = [recursive_nth_fibo(num) for num in range(n+1)]
+    print(sekvencia)
+
     pass
 
 
